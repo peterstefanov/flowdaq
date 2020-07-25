e7f19062763a853c6ee34e8078e73f5c047f605a
Peter Stefanov <petar.stefanov@leaseplan.com>
2020-07-25 18:16:58 +0100

    delivery to cooler

--- webui/src/app/pages/devices/devices.component.ts
+++ webui/src/app/pages/devices/devices.component.ts
@@ -6,7 +6,8 @@ import { UserInfoService             } from '../../services/user-info.service';
 
 import { Device                    } from '../../models/device';
 
-
+import { Delivery                    } from '../../models/delivery';
+import { DeliveryService             } from '../../services/api/delivery.service';
 
 @Component({
 	selector: 'f-devices-pg',
@@ -26,11 +27,18 @@ export class DevicesComponent implements OnDestroy {
     isToggled: boolean = false;
     selected = [];;
     public customerName: string = "";
+    userRole: string = "";
     
     ColumnMode = ColumnMode;
     SelectionType = SelectionType;
+           
+    public deliveryErrorMsg: string = '';
+    public deliverySuccessMsg: string = '';
     
-    constructor(private router: Router, private deviceService: DeviceService, private userInfoService: UserInfoService) {
+    createDeliveryObject: Delivery = {id: 0, status: '', fromDistributorId: 0, fromCustomerId: 0, fromFacilityId: 0, toCustomerId: 0, toFacilityId: 0, toCoolerId: 0, driverId: 0, vehicleId: 0, deliveryDate: new Date(), actualDeliveryDate: new Date(), fullBottles: 0, actualFullsDelivered: 0, routeId: 0, emptiesRetrieved: 0, actualEmptiesRetrieved: 0, deliveryNotes: ''} as Delivery;
+    deliveryCreateModal = false;
+    
+    constructor(private router: Router, private deviceService: DeviceService, private userInfoService: UserInfoService, private deliveryService: DeliveryService) {
         this.navigationSubscription = this.router.events.subscribe((e: any) => {
             // If it is a NavigationEnd event re-initalise the component
             if (e instanceof NavigationEnd) {
@@ -38,6 +46,7 @@ export class DevicesComponent implements OnDestroy {
             }
         });
         this.customerName = this.userInfoService.getCustomerCompany();
+        this.userRole = this.userInfoService.getUserRole();
     }
     
     getPageData() {
@@ -62,12 +71,52 @@ export class DevicesComponent implements OnDestroy {
        console.log('Edit device');     
        console.log(row);       
     }     
-
-    createDelivery(row) {
-       console.log('create delivery');     
-       console.log(row);  
-    } 
     /**END Device action*/
+    
+   /* Delivery action */       
+    createDelivery(row): void {
+        /**TODO Based on the role defined to and from. Same for the dialog */
+        this.createDeliveryObject = {id: 0, status: 'SCHEDULED', fromDistributorId: 0, fromCustomerId: this.userInfoService.getCustomerId(), fromFacilityId: 0, toCustomerId: row.customerId, toFacilityId: 0, toCoolerId: 0, driverId: 0, vehicleId: 0, deliveryDate: new Date(), actualDeliveryDate: new Date(), fullBottles: 0, actualFullsDelivered: 0, routeId: 0, emptiesRetrieved: 0, actualEmptiesRetrieved: 0, deliveryNotes: ''};
+        this.deliveryCreateModal = true;
+    }
+     
+   /* Delivery dialog */      
+    public saveDelivery(): void {
+        this.deliverySuccessMsg = '';
+        this.deliveryErrorMsg = '';
+
+        var deliveryDate = new Date(this.createDeliveryObject.deliveryDate);
+        var deliveryDateFormatted = new Date(deliveryDate.getTime() - (deliveryDate.getTimezoneOffset() * 60000 )).toISOString().split("T")[0];
+        this.createDeliveryObject.deliveryDate = new Date(deliveryDateFormatted);
+        /**set actual delivery date as the intended delivery date*/
+        this.createDeliveryObject.actualDeliveryDate = new Date(deliveryDateFormatted);
+   
+        this.deliveryService.createDelivery(this.createDeliveryObject)
+            .subscribe(resp => {
+                if (resp.success === false) {
+                    this.deliveryErrorMsg = resp.message;
+                    return;
+                } else if (resp.success === true) {
+                    this.cancelDelivery();
+                    this.deliverySuccessMsg = resp.message;
+                    /**On success unselect the user and reload the table*/
+                    this.remove();
+                    this.getPageData();
+                    return;
+                 }              
+             }
+         );
+    }
+
+    public cancelDelivery(): void {
+        this.deliveryCreateModal = false;
+    }
+    
+    public closeDeliverySuccess(): void {
+        this.deliverySuccessMsg = '';
+    }
+    /* END Delivery dialog */
+    /* END Delivery action */
     
     toggleExpandRow(row) {
         console.log('Toggled Expand Row!', row);
